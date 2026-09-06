87665c6c13a29cbeee9dcf7200a5bf712d73b42a
EnderJack379 <leonelcussi9@gmail.com>
2026-09-06 18:38:06 -0300

    Implementación de prueba unitaria para PedidoRepository

--- src/test/java/com/irisdual/pedidos_api/PedidosApiApplicationTests.java
+++ src/test/java/com/irisdual/pedidos_api/PedidosApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.irisdual.pedidos_api;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.irisdual.pedidos_api.repository.PedidoRepository;
 
 @SpringBootTest
 class PedidosApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
+    @Test
+    void verificarInyeccionRepositorio() {
+        // Valida que el repositorio se conecte correctamente a la base de datos
+        assertNotNull(pedidoRepository, "El repositorio de pedidos debe estar instanciado en el contexto");
+    }
 }
